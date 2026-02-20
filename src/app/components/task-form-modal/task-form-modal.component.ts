5236167fd5174f1f636ef2baa7b716b6a5ffd09f
prikasouzadev <prikasouza.dev@gmail.com>
2026-02-20 14:44:00 -0300

    Retornando dados ao fechar o modal de criação e edição

--- src/app/components/task-form-modal/task-form-modal.component.ts
+++ src/app/components/task-form-modal/task-form-modal.component.ts
@@ -1,7 +1,8 @@
-import { DIALOG_DATA } from '@angular/cdk/dialog';
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
 import { Component, inject } from '@angular/core';
 import { ITaskFormModalData } from '../../interfaces/task-form-modal-data.interface';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ITaskFormControls } from '../../interfaces/task-form-controls.interface';
 
 
 @Component({
@@ -12,15 +13,22 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class TaskFormModalComponent {
   readonly _data: ITaskFormModalData = inject (DIALOG_DATA);
+  readonly dialogRef = inject(DialogRef)
 
   taskForm : FormGroup =  new FormGroup({
     name: new FormControl(this._data.formValues.name, [Validators.required, Validators.minLength(10)]),
-    
-    description: new FormControl(this._data.formValues.desription, [Validators.required, Validators.minLength(50)])
+
+    description: new FormControl(this._data.formValues.desription, [Validators.required, Validators.minLength(10)])
   })
 
   onFormSubmit() {
-
+    this.closeModal(this.taskForm.value)
   }
+
+  closeModal(formValues: ITaskFormControls | undefined = undefined){
+    this.dialogRef.close(formValues);
+  }
+
+  
  
 }
--- src/app/services/modal-controller.service.ts
+++ src/app/services/modal-controller.service.ts
@@ -15,8 +15,9 @@ export class ModalControllerService{
     private readonly _dialog = inject(Dialog);
 
     openNewTaskModal(){
-        return this._dialog.open(TaskFormModalComponent, {
+        return this._dialog.open<ITaskFormControls>(TaskFormModalComponent, {
             ...this.modalSizeOptions,
+            disableClose: true,
             data: {
                 mode: 'create',
                 formValues:{
@@ -28,8 +29,9 @@ export class ModalControllerService{
     }
 
     openEditTaskModal(formValues: ITaskFormControls){
-        return this._dialog.open(TaskFormModalComponent,{
+        return this._dialog.open<ITaskFormControls>(TaskFormModalComponent,{
             ...this.modalSizeOptions,
+            disableClose: true,
             data: {
                 mode: 'edit',
                 formValues,
