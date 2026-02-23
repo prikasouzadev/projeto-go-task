14936ef770e158dac79a2a2079772e3e6188390f
prikasouzadev <prikasouza.dev@gmail.com>
2026-02-23 17:34:54 -0300

    Renderizando as tarefas com AsyncPipe e @let

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -3,30 +3,20 @@ import { TaskCardComponent } from "../task-card/task-card.component";
 import { TaskService } from '../../services/task.service';
 import { CdkDrag, CdkDragDrop, CdkDropList , moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop'
 import { ITask } from '../../interfaces/task.interface';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [TaskCardComponent, CdkDropList, CdkDrag ],
+  imports: [TaskCardComponent, CdkDropList, CdkDrag , AsyncPipe ],
   templateUrl: './task-list-section.component.html',
   styleUrl: './task-list-section.component.css'
 })
 export class TaskListSectionComponent {
-  todoTasks: ITask[] = [];
-  doingTasks: ITask[] = [];
-  doneTasks: ITask[] = [];
-
-   private readonly _taskService = inject(TaskService);
+  
+   readonly _taskService = inject(TaskService);
 
   ngOnInit() {
-    this._taskService.todoTasks.subscribe((todoList) => {
-      this.todoTasks = todoList;
-    });
-    this._taskService.doingTasks.subscribe((doingTasks) => {
-      this.doingTasks = doingTasks;
-    });
-    this._taskService.doneTasks.subscribe((doneTasks) => {
-      this.doneTasks = doneTasks;
-    });
+    
   }
 
   drop(event: CdkDragDrop<ITask[]>) {
